8961977e8da1d299478105d2295bee97e4d21660
ACRivas <ac.rivas.dev@gmail.com>
2023-01-12 17:17:29 -0300

    First main branch commit

---
+++ src/gitTest1/GitTest1.java
@@ -0,0 +1,15 @@
+package gitTest1;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		
+		String works = "yes";
+		
+		if (works == "yes") {
+			System.out.println("Well done!");
+		}
+
+	}
+
+}
